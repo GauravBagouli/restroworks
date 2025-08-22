37e6ab3dca161330214e816177fde2e9923c2e1c
Gaurav Bagouli <gaurav.bagouli@techsuperiors.com>
2025-08-23 02:18:39 +0530

    next config update

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -11,6 +11,11 @@ const nextConfig = {
         protocol: "https",
         hostname: "www.restroworks.com",
       },
+      {
+        protocol: "https",
+        hostname: "restroworks.onrender.com",
+        pathname: "/api/media/file/**",
+      },
     ],
     formats: ["image/avif", "image/webp"],
     minimumCacheTTL: 60,
